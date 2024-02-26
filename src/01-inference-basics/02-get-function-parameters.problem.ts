7b138136a822ae6ab115e6d40699b55740c1f011
Mario Lazzari <mario.lazzari@gmail.com>
2024-02-26 21:39:21 +0100

    Done ex2

---
+++ src/01-inference-basics/02-get-function-parameters.problem.ts
@@ -0,0 +1,36 @@
+import { Equal, Expect } from "../helpers/type-utils";
+
+const makeQuery = (
+  url: string,
+  opts?: {
+    method?: string;
+    headers?: {
+      [key: string]: string;
+    };
+    body?: string;
+  }
+) => {};
+
+type MakeQueryParameters = Parameters<typeof makeQuery>;
+
+const test = () => {};
+
+type Test = typeof test;
+
+type tests = [
+  Expect<
+    Equal<
+      MakeQueryParameters,
+      [
+        url: string,
+        opts?: {
+          method?: string;
+          headers?: {
+            [key: string]: string;
+          };
+          body?: string;
+        }
+      ]
+    >
+  >
+];
