8d2cdea3fab73f4ff963e95d3000a87e741c5a4e
Mario Lazzari <mario.lazzari@gmail.com>
2024-02-29 23:05:38 +0100

    Done lesson 77

--- src/04-conditional-types-and-infer/26-get-result-from-async-function.problem.ts
+++ src/04-conditional-types-and-infer/26-get-result-from-async-function.problem.ts
@@ -3,6 +3,7 @@ import { Equal, Expect } from "../helpers/type-utils";
 const getServerSideProps = async () => {
   const data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
   const json: { title: string } = await data.json();
+
   return {
     props: {
       json,
@@ -10,7 +11,11 @@ const getServerSideProps = async () => {
   };
 };
 
-type InferPropsFromServerSideFunction = unknown;
+type InferPropsFromServerSideFunction<T> = T extends () => Promise<{
+  props: infer Props;
+}>
+  ? Props
+  : never;
 
 type tests = [
   Expect<
